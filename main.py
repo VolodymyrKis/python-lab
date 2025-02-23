7c70608d4d7b114081ac3c77e04a311f7ae82da8
Volodymyr <kis.volodymyr1@gmail.com>
2025-02-23 20:35:21 +0200

    irst

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Volodymydsdr')
+    print_hi('Volodymydвівіsdr')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
